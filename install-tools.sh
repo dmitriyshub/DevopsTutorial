bd65bf6f5b91ececd07dd2a961e645fa69429a12
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-23 13:16:04 +0300

    java fix

--- install-tools.sh
+++ install-tools.sh
@@ -1,14 +1,16 @@
 #!/bin/bash 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install software-properties-common -y 
 sudo apt-add-repository ppa:ansible/ansible  
-sudo apt-get update  
+sudo apt-get update -y
 sudo apt-get install ansible -y 
-sudo apt install default-jre -y
-sudo apt install default-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update 
+sudo apt install openjdk-8-jre -y
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 #sudo dpkg --configure -a
-sudo apt-get install jenkins -y 
 sudo apt-get install awscli -y

1f7a3eb1ce7d6111eaeaa9ca1bd00e5711e4a216
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-24 12:24:58 +0300

    saved files*

--- install-tools.sh
+++ install-tools.sh
@@ -29,3 +29,14 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 sudo apt-get update 
 sudo apt-get install jenkins -y 
 sudo apt-get install awscli -y
+
+ #docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+sudo usermod -aG docker jenkins
+sudo systemctl enable docker
+sudo apt install git -y

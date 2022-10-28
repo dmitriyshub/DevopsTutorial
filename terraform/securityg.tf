861cb0c3868027fc02235c030a9eccd80a751382
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-28 13:48:08 +0300

    security group file

---
+++ terraform/securityg.tf
@@ -0,0 +1,48 @@
+// Creating aws security resource
+resource "aws_security_group" "allow_tcp" {
+  name        = "allow_tcp1"
+  description = "Allow TCP inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "TCP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Jenkins from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+      Name = "TCP allow"
+      Pro = var.my_tag
+      Env = var.my_tag_env
+  }
+}

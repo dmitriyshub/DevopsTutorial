8506c4e8edf7cd87ce9905115c068c69db5aa145
dmitriyshub <d.shtranvasser@gmail.com>
2022-10-30 15:45:51 +0200

    add new stage for testing

--- terraform/setupVol.tf
+++ terraform/setupVol.tf
@@ -14,10 +14,23 @@
 resource "null_resource" "setupVol" {
   depends_on = [
     aws_efs_mount_target.mountefs,
+    aws_cloudfront_distribution.s3_distribution,
   ]
 
   //
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/dimash/Temp/${var.key_name}.pem -i '${aws_instance.myWebOS.public_ip},' master.yml -e 'file_sys_id=${aws_efs_file_system.myWebEFS.id}'"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/dimash/Temp/${var.key_name}.pem -i '${aws_instance.myWebOS.public_ip},' master2.yml -e 'file_sys_id=${aws_efs_file_system.myWebEFS.id}'"
+  }
+}
+
+// Configuring the external volume
+resource "null_resource" "setupVol2" {
+  depends_on = [
+    null_resource.setupVol,
+  ]
+
+  //
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/dimash/Temp/${var.key_name}.pem -i '${aws_instance.myWebOS.public_ip},' master3.yml --extra-vars \"file_system_dns=${aws_efs_file_system.myWebEFS.dns_name}\""
   }
 }
